693e3dc493c93af83672f57439a266c476e05f7b
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-07-20 18:23:35 +0530

    Merge

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -3,7 +3,7 @@ import landingPageBanner from '../assets/landingPage/mainBanner2.jpg'
 
 const Hero = () => {
     return (
-        <section className=' w-full h-fit flex bg-slate-500'>
+        <section className=' w-full h-[100vh] overflow-hidden flex bg-slate-500'>
             <div className='w-full bg-black'>
                 <img src={landingPageBanner} className='-z-10 opacity-50' alt='banner_image' />
             </div>

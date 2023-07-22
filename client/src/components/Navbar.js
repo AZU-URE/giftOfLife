12a1189ca60f9ddb309f113c64aa6f807e33f3e2
AZU-URE <202051069@iiitvadodara.ac.in>
2023-07-22 15:12:33 +0530

    Registration form added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from '../utils/connect'
 import useConnect from '../hook/useConnect'
+import { BiSolidUser } from 'react-icons/bi'
 
 const Navbar = ({ aboutRef, ReceiversRef, DoctorRef }) => {
-    const { setAccount, account } = useConnect()
+    const { setAccount, account, setContract } = useConnect()
+    const [address, setAddress] = useState()
 
     const clickAbout = () => {
         aboutRef.current?.scrollIntoView({ behaviour: 'smooth' })
@@ -19,11 +21,19 @@ const Navbar = ({ aboutRef, ReceiversRef, DoctorRef }) => {
         try {
             const { contract, provider, signer } = await connect()
             setAccount({ contract, provider, signer })
+            setContract(contract)
         }
         catch (e) {
             console.log(e);
         }
     }
+
+    useEffect(() => {
+        // console.log(account?.signer?.address);
+        setAddress(account?.signer?.address);
+    }, [account])
+
+
     return (
         <>
             <nav className='fixed top-0 z-20 bg-slate-400 bg-opacity-50 w-full backdrop-filter backdrop-blur-2xl'>
@@ -34,9 +44,18 @@ const Navbar = ({ aboutRef, ReceiversRef, DoctorRef }) => {
                         <li onClick={clickPatients}>Patients</li>
                         <li onClick={clickDoctors}>Doctors</li>
                     </ul>
-                    <div className='flex space-x-3'>
-                        <button onClick={handleConnect} className='bg-mainGreen rounded-md hover:bg-mainGreen/80 text-white p-2 px-3.5 font-medium'>Connect</button>
-                        <button className='bg-transparent rounded-md hover:bg-mainGreen/80 hover:text-white border-[2px] border-mainGreen text-mainGreen p-2 px-3.5 font-medium'>SignUp</button>
+                    <div className='flex space-x-3 items-center'>
+                        {
+                            address
+                                ?
+                                <div className='flex space-x-2 items-center' >
+                                    <BiSolidUser style={{ fontSize: '24px' }} className='text-mainGreen bg-white rounded-full p-1'></BiSolidUser>
+                                    <p className='text-white '>{address.substring(0, 6)}...{address.substring(4, -1)}</p>
+                                </div>
+                                :
+                                <button onClick={handleConnect} className='bg-mainGreen rounded-md hover:bg-mainGreen/80 text-white p-2 px-3.5 font-medium'>Connect</button>
+                        }
+                        {/* <button className='bg-transparent rounded-md hover:bg-mainGreen/80 hover:text-white border-[2px] border-mainGreen text-mainGreen p-2 px-3.5 font-medium'>SignUp</button> */}
                         {/* <button className='bg-mainGreen rounded-md p-0.5 font-medium'><p className='bg-slate-800 rounded-md p-1.5 px-3 hover:text-white text-mainGreen hover:bg-mainGreen'>SignUp</p></button> */}
                     </div>
                 </div>
---
+++ client/src/components/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterForm from './forms/RegisterForm'
+
+const Register = () => {
+    return (
+        <div className='flex items-center justify-center bg-mixingGreen/20 min-h-screen w-full'>
+            <RegisterForm />
+        </div>
+    )
+}
+
+export default Register
